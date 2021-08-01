fccc255d01d64705bb7ef0c004180ee7de6239da
Dev9tkov <idev9tkov@gmail.com>
2021-08-01 11:56:40 +0500

    13.
    Mono How it works and write Junit Tests

--- src/main/java/com/learnreactivespring/fluxandmono/FluxAndMonoTest.java
+++ src/main/java/com/learnreactivespring/fluxandmono/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.learnreactivespring.fluxandmono;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
@@ -61,6 +62,21 @@ public class FluxAndMonoTest {
         StepVerifier.create(stringFlux)
                 .expectNext("Spring", "Spring Boot", "Reactive Spring")
                 .expectErrorMessage("Exception Occured")
+                .verify();
+    }
+
+    @Test
+    public void monoTest() {
+        Mono<String> stringMono = Mono.just("Spring");
+        StepVerifier.create(stringMono.log())
+                .expectNext("Spring")
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoTest_Error() {
+        StepVerifier.create(Mono.error(new RuntimeException("Exception Occured")).log())
+                .expectError(RuntimeException.class)
                 .verify();
     }
 }
