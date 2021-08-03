112ec5578511cb59d75fa383078c7b8e0895d19b
Dev9tkov <idev9tkov@gmail.com>
2021-08-03 19:34:13 +0500

    54.
    Create an Item EndPoint

---
+++ src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -0,0 +1,77 @@
+package com.learnreactivespring.controller.v1;
+
+import com.learnreactivespring.constants.ItemConstants;
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.context.annotation.ApplicationScope;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("nonprod")
+public class ItemControllerTest {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+        return Arrays.asList(new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 329.99),
+                new Item(null, "Apple Watch", 349.99),
+                new Item("ABC", "Beats HeadPhones", 19.99));
+    }
+
+    @Before
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> {
+                    System.out.println("Insert item is : " + item);
+                })
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void createItem() {
+        Item item = new Item(null, "Iphone X", 999.9);
+        webTestClient.post().uri(ItemConstants.ITEM_END_POINT_V1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.description").isEqualTo("Iphone X")
+                .jsonPath("$.price").isEqualTo(999.9);
+    }
+}
